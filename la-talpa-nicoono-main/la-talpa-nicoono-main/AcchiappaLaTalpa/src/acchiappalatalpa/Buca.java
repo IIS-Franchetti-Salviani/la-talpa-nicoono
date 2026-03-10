370ec7db352f5084af73892402d17747c7fb53f2
nicoono <onorati.nicolo@franchettisalviani.net>
2026-03-10 10:58:27 +0100

    sistemazione alcuni errori

--- la-talpa-nicoono-main/la-talpa-nicoono-main/AcchiappaLaTalpa/src/acchiappalatalpa/Buca.java
+++ la-talpa-nicoono-main/la-talpa-nicoono-main/AcchiappaLaTalpa/src/acchiappalatalpa/Buca.java
@@ -4,11 +4,42 @@
  */
 package acchiappalatalpa;
 
+import java.awt.Color;
 /**
  *
  * @author onorati.nicolo
  */
 public class Buca {
+    private boolean occupata;
+    private RoundButton bottone;
+    private final Color COLORE_TALPA = new Color(139,69,19); 
+    private final Color COLORE_VUOTO = Color.LIGHT_GRAY;
 
+    public Buca(boolean occupata, RoundButton bottone) {
+        this.occupata = false;
+        this.bottone = bottone;
+        this.bottone.setBackground(COLORE_VUOTO);
+    }
+    
+    public void setOccupata(boolean stato){
+        this.occupata = stato;
+        
+        if(stato){
+            bottone.setBackground(COLORE_TALPA);
+        }
+        else{
+            bottone.setBackground(COLORE_VUOTO);
+        }
+    }
+    
+    public boolean isOccupata(){
+        return occupata;
+    }
+    
+    
+    public RoundButton getBottone(){
+        return bottone;
+    }
+    
     
 }
